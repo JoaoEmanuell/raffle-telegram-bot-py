f4a8616462e9dd1100026ba6471e2f535c502cf9
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-05 11:33:11 -0300

    feat: production

--- raffle_telegram_bot/src/db/connection.py
+++ raffle_telegram_bot/src/db/connection.py
@@ -1,7 +1,5 @@
-from pathlib import Path
-
 from sqlalchemy import create_engine
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+from ..utils import database_path
 
-db_engine = create_engine(f"sqlite:///{BASE_DIR}/database/telegram_bot.db", echo=True)
+db_engine = create_engine(f"sqlite:///{database_path}/telegram_bot.db")
--- raffle_telegram_bot/src/db/create_tables.py
+++ raffle_telegram_bot/src/db/create_tables.py
@@ -3,6 +3,4 @@ from .models import Base
 
 
 def create_tables() -> None:
-    # metadata = MetaData()
-    # metadata.create_all(bind=db_engine, tables=[RaffleModel])
     Base.metadata.create_all(bind=db_engine)
